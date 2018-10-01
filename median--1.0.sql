7e560e6c67aef1746704764bfd6c17367d8b543d
Colin Stolley <cstolley@runbox.com>
2018-10-01 13:25:13 -0500

    Handle timestamp and character types, clean-up.

--- median--1.0.sql
+++ median--1.0.sql
@@ -1,11 +1,13 @@
-CREATE OR REPLACE FUNCTION _median_transfn(state internal, double precision)
+CREATE OR REPLACE FUNCTION _median_transfn(state internal, val anyelement)
 RETURNS internal
 AS 'MODULE_PATHNAME', 'median_transfn'
 LANGUAGE C IMMUTABLE;
 
-CREATE OR REPLACE FUNCTION _median_finalfn(state internal, double precision)
+/* numeric types coerced to float8's */
+
+CREATE OR REPLACE FUNCTION _median_double_finalfn(state internal, val double precision)
 RETURNS double precision
-AS 'MODULE_PATHNAME', 'median_finalfn'
+AS 'MODULE_PATHNAME', 'median_double_finalfn'
 LANGUAGE C IMMUTABLE;
 
 DROP AGGREGATE IF EXISTS median (double precision);
@@ -13,6 +15,40 @@ CREATE AGGREGATE median (double precision)
 (
     sfunc = _median_transfn,
     stype = internal,
-    finalfunc = _median_finalfn,
+    finalfunc = _median_double_finalfn,
     finalfunc_extra
 );
+
+/* timestamp with timezone */
+
+CREATE OR REPLACE FUNCTION _median_timestamptz_finalfn(state internal, val timestamptz)
+RETURNS timestamptz
+AS 'MODULE_PATHNAME', 'median_timestamptz_finalfn'
+LANGUAGE C IMMUTABLE;
+
+DROP AGGREGATE IF EXISTS median (timestamptz);
+CREATE AGGREGATE median (timestamptz)
+(
+    sfunc = _median_transfn,
+    stype = internal,
+    finalfunc = _median_timestamptz_finalfn,
+    finalfunc_extra
+);
+
+
+/* strings */
+
+CREATE OR REPLACE FUNCTION _median_text_finalfn(state internal, val text)
+RETURNS text
+AS 'MODULE_PATHNAME', 'median_text_finalfn'
+LANGUAGE C IMMUTABLE;
+
+DROP AGGREGATE IF EXISTS median (text);
+CREATE AGGREGATE median (text)
+(
+    sfunc = _median_transfn,
+    stype = internal,
+    finalfunc = _median_text_finalfn,
+    finalfunc_extra
+);
+
--- median.c
+++ median.c
@@ -1,133 +1,217 @@
+/*-------------------------------------------------------------------------
+ *
+ * median.c
+ *    A median aggregate implementation using a sorted array.
+ *
+ *-------------------------------------------------------------------------
+ */
+
 #include <postgres.h>
 #include <fmgr.h>
 #include <math.h>
+#include "utils/float.h"
+#include "utils/timestamp.h"
+#include "utils/varlena.h"
+
+
 
 #ifdef PG_MODULE_MAGIC
 PG_MODULE_MAGIC;
 #endif
+typedef struct varlena VarString;
+#define DatumGetVarStringPP(X)                ((VarString *) PG_DETOAST_DATUM_PACKED(X))
 
 
 typedef struct
 {
-    int    alloc_len;      /* allocated length */
-    int    next_alloc_len; /* next allocated length */
-    int    nelems;            /* number of valid entries */
-    union
-    {
-        float4    *float4_values;
-        float8  *float8_values;
-    } d;
-} State;
+	Size		alloc_len;		/* allocated length */
+	Size		next_alloc_len; /* next allocated length */
+	Size		nelems;			/* number of valid entries */
+	Datum	   *d;
+}			State;
 
-int float8_cmp(const void *_a, const void *_b);
+int			float8_cmp(const void *a, const void *b);
+int			timestamp_cmp(const void *a, const void *b);
+int			text_cmp(const void *a, const void *b);
 
 
 PG_FUNCTION_INFO_V1(median_transfn);
-
-
-
 /*
  * Median state transfer function.
  *
- * This function is called for every value in the set that we are calculating
- * the median for. On first call, the aggregate state, if any, needs to be
- * initialized.
- */
+ * This function accumulates all results into an unsorted array of
+ * Datums and expands * that array as needed. All results must be
+ * loaded into memory (no spilling to disk).
+ * */
 Datum
 median_transfn(PG_FUNCTION_ARGS)
 {
-    MemoryContext   agg_context;
-    State          *state = (State*)(PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
-    double          value;
-    
-    
+	MemoryContext agg_context;
+	State	   *state = (State *) (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+	Datum		value;
 
-    if (!AggCheckCallContext(fcinfo, &agg_context))
-        elog(ERROR, "median_transfn called in non-aggregate context");
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_transfn called in non-aggregate context");
 
-    if (PG_ARGISNULL(1))
-        PG_RETURN_BYTEA_P(state);
+	if (PG_ARGISNULL(1))
+		PG_RETURN_BYTEA_P(state);
 
-    value = PG_GETARG_FLOAT8(1);
+	value = PG_GETARG_DATUM(1);
 
 
-    if (state == NULL)
-    {
-        state = MemoryContextAllocZero(agg_context, sizeof(State));
-        state->d.float8_values = MemoryContextAllocZero(agg_context, 1024 * sizeof(float8));
-        state->alloc_len = 1024;
-        state->next_alloc_len = 2048;
-        state->nelems = 0;
-    }
-    else if (state->nelems >= state->alloc_len)
-    {
-        MemoryContext   old_context = MemoryContextSwitchTo(agg_context);
-        int             newlen = state->next_alloc_len;
+	if (state == NULL)
+	{
+		state = MemoryContextAllocZero(agg_context, sizeof(State));
+		state->d = MemoryContextAllocZero(agg_context, sizeof(Datum) * 1024);
+		state->alloc_len = 1024;
+		state->next_alloc_len = 2048;
+		state->nelems = 0;
+	}
+	else if (state->nelems >= state->alloc_len)
+	{
+		MemoryContext old_context = MemoryContextSwitchTo(agg_context);
+		int			newlen = state->next_alloc_len;
 
-        state->next_alloc_len += state->alloc_len;
-        state->alloc_len = newlen;
-        state->d.float4_values = repalloc(state->d.float8_values,
-                                          state->alloc_len * sizeof(float8));
-        MemoryContextSwitchTo(old_context);
-    }
+		state->next_alloc_len += state->alloc_len;
+		state->alloc_len = newlen;
+		state->d = repalloc(state->d, state->alloc_len * sizeof(Datum));
+		MemoryContextSwitchTo(old_context);
+	}
 
-    /* elog(INFO, "Accum value (%d) %2.2f", state->nelems, value); */
+	state->d[state->nelems++] = value;
 
-    state->d.float8_values[state->nelems++] = value;
-
-    PG_RETURN_BYTEA_P(state);
+	PG_RETURN_BYTEA_P(state);
 }
 
-PG_FUNCTION_INFO_V1(median_finalfn);
-
-/*
- * Median final function.
- *
- * This function is called after all values in the median set has been
- * processed by the state transfer function. It should perform any necessary
- * post processing and clean up any temporary state.
- */
-Datum
-median_finalfn(PG_FUNCTION_ARGS)
+/* type-specific comparison functions */
+int
+float8_cmp(const void *a, const void *b)
 {
-    MemoryContext agg_context;
-    State          *state = (State*)(PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+	float8		af = DatumGetFloat8(*(Datum *) a);
+	float8		bf = DatumGetFloat8(*(Datum *) b);
 
-    if (!AggCheckCallContext(fcinfo, &agg_context))
-        elog(ERROR, "median_finalfn called in non-aggregate context");
-
-    if (state == NULL)
-        PG_RETURN_NULL();
-
-    qsort(state->d.float8_values, state->nelems, sizeof(float8), float8_cmp);
-
-    PG_RETURN_FLOAT8(state->d.float8_values[state->nelems / 2 + 1 - 1]);
+	return float8_cmp_internal(af, bf);
 }
 
 int
-float8_cmp(const void *_a, const void *_b)
+timestamp_cmp(const void *a, const void *b)
 {
-    float8 a = *((float8 *) _a);
-    float8 b = *((float8 *) _b);
+	Timestamp	at = DatumGetTimestamp(*(Datum *) a);
+	Timestamp	bt = DatumGetTimestamp(*(Datum *) b);
 
-    if (isnan(a))
-    {
-        if (isnan(b))
-            return 0;
-        else
-            return 1;
-    }
-    else if (isnan(b))
-    {
-        return -1;
-    }
-    else
-    {
-        if (a > b)
-            return 1;
-        else if (a < b)
-            return -1;
-        else
-            return 0;
-    }
+	return timestamptz_cmp_internal(at, bt);
+}
+
+int
+text_cmp(const void *a, const void *b)
+{
+	Datum		ad = *(Datum *) a;
+	Datum		bd = *(Datum *) b;
+	struct varlena *arg1 = DatumGetVarStringPP(ad);
+	struct varlena *arg2 = DatumGetVarStringPP(bd);
+	char	   *a1p,
+			   *a2p;
+	int			len1,
+				len2,
+				result;
+
+	a1p = VARDATA_ANY(arg1);
+	a2p = VARDATA_ANY(arg2);
+
+	len1 = VARSIZE_ANY_EXHDR(arg1);
+	len2 = VARSIZE_ANY_EXHDR(arg2);
+
+	result = memcmp(a1p, a2p, Min(len1, len2));
+	if ((result == 0) && (len1 != len2))
+		result = (len1 < len2) ? -1 : 1;
+
+	/* We can't afford to leak memory here. */
+	if (PointerGetDatum(arg1) != ad)
+		pfree(arg1);
+	if (PointerGetDatum(arg2) != bd)
+		pfree(arg2);
+
+	return result;
+}
+
+PG_FUNCTION_INFO_V1(median_double_finalfn);
+
+/*
+ * Median final function for doubles.
+ *
+ * This sorts the accumulated array of datums and returns the median.
+ */
+Datum
+median_double_finalfn(PG_FUNCTION_ARGS)
+{
+	MemoryContext agg_context;
+	State	   *state = (State *) (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+	Size		low_idx,
+				hi_idx;
+
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_finalfn called in non-aggregate context");
+
+	if (state == NULL)
+		PG_RETURN_NULL();
+
+	qsort(state->d, state->nelems, sizeof(Datum), float8_cmp);
+
+	low_idx = state->nelems / 2;
+	hi_idx = state->nelems / 2 - 1;
+
+	if (low_idx == hi_idx)
+		PG_RETURN_FLOAT8(DatumGetFloat8(state->d[low_idx]));
+	else
+		PG_RETURN_FLOAT8(
+						 (DatumGetFloat8(state->d[low_idx]) +
+						  DatumGetFloat8(state->d[hi_idx])) / 2);
+}
+
+PG_FUNCTION_INFO_V1(median_timestamptz_finalfn);
+
+/*
+ * Median final function for timestamps.
+ *
+ * This sorts the accumulated array of datums and returns the median.
+ */
+Datum
+median_timestamptz_finalfn(PG_FUNCTION_ARGS)
+{
+	MemoryContext agg_context;
+	State	   *state = (State *) (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_finalfn called in non-aggregate context");
+
+	if (state == NULL)
+		PG_RETURN_NULL();
+
+	qsort(state->d, state->nelems, sizeof(Datum), timestamp_cmp);
+
+	PG_RETURN_TIMESTAMPTZ(DatumGetTimestamp(state->d[state->nelems / 2]));
+}
+
+PG_FUNCTION_INFO_V1(median_text_finalfn);
+
+/*
+ * Median final function for text.
+ *
+ * This sorts the accumulated array of datums and returns the median.
+ */
+Datum
+median_text_finalfn(PG_FUNCTION_ARGS)
+{
+	MemoryContext agg_context;
+	State	   *state = (State *) (PG_ARGISNULL(0) ? NULL : PG_GETARG_BYTEA_P(0));
+
+	if (!AggCheckCallContext(fcinfo, &agg_context))
+		elog(ERROR, "median_finalfn called in non-aggregate context");
+
+	if (state == NULL)
+		PG_RETURN_NULL();
+
+	qsort(state->d, state->nelems, sizeof(Datum), text_cmp);
+
+	PG_RETURN_TEXT_P(DatumGetTextPP(state->d[state->nelems / 2]));
 }
